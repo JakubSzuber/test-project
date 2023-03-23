235d7f3a5c17f9af76cec57e71074d542ff9a3d9
Jakub Szuber <90647840+JakubSzuber@users.noreply.github.com>
2023-03-23 21:58:12 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require ('express')
-consts app = expose()
+const app = expose()
 
 app.get ('/', (req, res) => res.send('Hello World'))
 app.listen (3000, () => console.log('server ready'))
