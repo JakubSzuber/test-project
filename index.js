75f2befb7359d3850e308c7f2a9e43f61623f7fb
Jakub Szuber <90647840+JakubSzuber@users.noreply.github.com>
2023-03-25 19:36:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const express =require('express')
-const app=express()
-
-
-
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => res.send('Hello World'))
 app.listen(80, () => console.log('server ready'))
